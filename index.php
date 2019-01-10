d94dd317500d6d96912fab5ccf0f98628cbfa003
llonguet <46523902+llonguet@users.noreply.github.com>
2019-01-10 15:16:02 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
-<?php
+<?php session_start();?>
+<!DOCTYPE html>
+	<html>
+		<head>
+				<title>Lecteur RFID</title>
+    			<?php 
+    					include 'class/TcpClient.php';
+    					include 'class/Bdd.php';
+    					$tcp = new TcpClient("","");
+    					
+    			?>
+		</head>
 
-test
+		<body>
+				<?php
+					$bdd = new Bdd("127.0.0.1","lecteur","root",""); // connexion a la bdd 
+					$reponse = $bdd->query('SELECT * FROM user ');
 
-test6
+					while ($resultat = $reponse->fetch())
 
-?>
+					//WHERE id = 671DD869
+					{
+						echo $resultat['id'] ;
+					}
+				?>
+		</body>
+
+
+
+</html>
